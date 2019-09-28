6e4859f9ba1a1dc8fda8246f6a1dc961a385ec1f
Arsen <arsen.simonyan.a@gmail.com>
2019-09-28 22:30:13 +0400

    first 1

---
+++ app/src/main/java/am/newway/aca/BaseActivity.java
@@ -0,0 +1,70 @@
+package am.newway.aca;
+
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    public final int CUSTOMIZED_REQUEST_CODE = 0x0000ffff;
+    private final String TAG = getClass().getSimpleName();
+
+    public void scanBarcode( View view) {
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    public void scanBarcodeWithCustomizedRequestCode(View view) {
+        new IntentIntegrator(this).setRequestCode(CUSTOMIZED_REQUEST_CODE).initiateScan();
+    }
+
+    public void scanBarcodeInverted(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.addExtra( Intents.Scan.SCAN_TYPE, Intents.Scan.INVERTED_SCAN);
+        integrator.initiateScan();
+    }
+
+    public void scanMixedBarcodes(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.addExtra(Intents.Scan.SCAN_TYPE, Intents.Scan.MIXED_SCAN);
+        integrator.initiateScan();
+    }
+
+    public void scanPDF417(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.PDF_417);
+        integrator.setPrompt("Scan something");
+        integrator.setOrientationLocked(false);
+        integrator.setBeepEnabled(false);
+        integrator.initiateScan();
+    }
+
+    public void scanBarcodeFrontCamera(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCameraId( Camera.CameraInfo.CAMERA_FACING_FRONT);
+        integrator.initiateScan();
+    }
+
+    public void generateQR(String text, ImageView view){
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(text, BarcodeFormat.QR_CODE, 400, 400);
+            view.setImageBitmap(bitmap);
+        } catch(Exception e) {
+            Log.e(TAG, ""+e.getMessage());
+        }
+    }
+
+    public void scanWithTimeout(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setTimeout(8000);
+        integrator.initiateScan();
+    }
+}
--- app/src/main/java/am/newway/aca/MainActivity.java
+++ app/src/main/java/am/newway/aca/MainActivity.java
@@ -1,19 +1,19 @@
 package am.newway.aca;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
@@ -21,11 +21,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
 
-        FloatingActionButton fab = findViewById( R.id.fab );
+        final FloatingActionButton fab = findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick ( View view ) {
-                Snackbar.make( view , "Replace with your own action" , Snackbar.LENGTH_LONG ).setAction( "Action" , null ).show();
             }
         } );
     }
@@ -39,9 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected ( MenuItem item ) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -50,5 +46,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != CUSTOMIZED_REQUEST_CODE && requestCode != IntentIntegrator.REQUEST_CODE) {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        switch (requestCode) {
+            case CUSTOMIZED_REQUEST_CODE: {
+                Toast.makeText(this, "REQUEST_CODE = " + requestCode, Toast.LENGTH_LONG).show();
+                break;
+            }
+            default:
+                break;
+        }
+
+        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+
+        if(result.getContents() == null) {
+            Toast.makeText(this, R.string.cancelled, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+        }
     }
 }
