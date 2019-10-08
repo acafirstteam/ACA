abd6b699f4f8657c776a3d7bc987055eff2090bb
Arsen <arsen.simonyan.a@gmail.com>
2019-10-08 22:17:32 +0400

    Merge branch 'master' of https://github.com/acafirstteam/ACA into Arsen-Simonyan
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/am/newway/aca/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    
    Կարգավորվել է ֆրագմենտների հերթականության հետ կապված խնդիրը
    Այժմ ամեն հետ քայլից հայտնվում ենք հիմնական ֆրագմենտի վրա
    որից հետո հետ քայլի դեպքում ստանում ենք զգուշացում փակվելու
    մասին և հաստատելուց հետո փակում ծրագիրը
    
    Ավելացել է Ադմին մասին Notofication մասը։ Նոր ուսանող հայտնաբերելու դեպքում համակարգը
    բոլոր ադմինիստրատորներին ուղարկում է հաղորդագրություն

--- app/src/main/java/am/newway/aca/MainActivity.java
+++ app/src/main/java/am/newway/aca/MainActivity.java
@@ -6,215 +6,189 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import am.newway.aca.database.Firestore;
-import am.newway.aca.googleauth.CreateAcountGoogle;
-import am.newway.aca.template.Course;
-import am.newway.aca.template.CoursesActivity;
-import am.newway.aca.ui.AccountActivity;
-
-import androidx.annotation.NonNull;
-
+import am.newway.aca.firebase.FirebaseLogin;
+import am.newway.aca.firebase.Firestore;
+import am.newway.aca.template.Student;
 import am.newway.aca.template.Visit;
 import am.newway.aca.ui.home.HomeFragment;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends BaseActivity implements FirebaseAuth.AuthStateListener {
+public
+class MainActivity extends BaseActivity {
 
-    private FirebaseAuth mAuth;
-    //private static long back_pressed;
-    private AppBarConfiguration mAppBarConfiguration;
-    private NavController navController;
-    private DrawerLayout drawer;
+    private static long back_pressed;
     private String TAG = getClass().getSimpleName();
-    //private MenuItem searchMenu;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+    protected
+    void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        initNavigationBar();
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.setTitle(R.string.nav_header_title);
-
-        final FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                if (acct == null) {
-
-                    startActivity(new Intent(MainActivity.this, CreateAcountGoogle.class));
-                } else {
-                    scanBarcode(view);
-                    Log.d(TAG, "Scan Bar code is ready");
-                }
-          /*       if (i==0){
-                     startActivity(new Intent(MainActivity.this, CreateAcountGoogle.class));
-                     Log.d(TAG,"int i ==1 that means start activity to log in");
-                 }
-                 else {
-                     scanBarcode( view );
-Log.d(TAG,"Scan Bar code is ready");
-                 }*/
-/*
-                if (firebaseAuth.getCurrentUser() == null) {
-                    startActivity(new Intent(MainActivity.this, CreateAcountGoogle.class));
-
-                }else {
-               scanBarcode( view );
-           }*/
-
-            }
-        });
-
-        //Կուրսերի ցանկի ներբեռնում
-        //        FIRESTORE.getCuorces( new Firestore.OnCourseReadListener() {
-        //            @Override
-        //            public void OnCourseRead ( final List<Course> courses ) {
-        //                for ( int i = 0; i != courses.size(); i++ ) {
-        //                    Log.e( "#################" , "OnCourseRead: " + courses.get( i ).getUrl() );
-        //                }
-        //            }
-        //        } );
-
-        //Նոր այցելության գրանցում
-        //        FIRESTORE.addNewVisit( ""+App.userID , "A1B2C3" , new Firestore
-        //        .OnVisitChangeListener() {
-        //            @Override
-        //            public void OnChangeConfirmed ( final Visit visit ) {
-        //
-        //            }
-        //        } );
-
-        drawer = findViewById(R.id.drawer_layout);
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_company, R.id.nav_about).setDrawerLayout(drawer).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int id = menuItem.getItemId();
-                navController.navigate(id);
-                //navController.popBackStack();
-                drawer.closeDrawer(GravityCompat.START);
-                return false;
-            }
-        });
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                if (fab.getVisibility() == View.VISIBLE) {
-                    fab.setTranslationX(slideOffset * 100);
-                    fab.setAlpha(1 - slideOffset);
-                }
-                super.onDrawerSlide(drawerView, slideOffset);
-            }
-        };
-
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-    }
-
-    /*public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        if (firebaseAuth.getCurrentUser() == null) {
-            startActivity(new Intent(MainActivity.this, CreateAcountGoogle.class));
+        if ( firebaseUser != null ) {
+            Log.e( TAG , "onCreate: " + firebaseUser.getUid() );
         }
-    }*/
-    @Override
-    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        else
+            Log.e( TAG , "onCreate: firebase is null" );
+
+        Student student = getGlobStudent();
 
 
-        if (firebaseAuth.getCurrentUser() == null) {
-            Log.d(TAG, "onAuthStateChanged is tru i=1");
-            //startActivity(new Intent(MainActivity.this, CreateAcountGoogle.class));
-        }
+        int nType = student.getType();
+        Log.e( TAG , "not nullik:  " +nType);
+        if ( nType == 2 )
+            addOnNewStudentListener();
 
+        fab = findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public
+            void onClick ( View view ) {
+
+                firebaseUser = mAuth.getCurrentUser();
+                if ( firebaseUser == null ) {
+                    Log.d( TAG , "Starting SignUp Activity" );
+                    startActivity( new Intent( MainActivity.this , FirebaseLogin.class ) );
+                }
+                else {
+                    Log.d( TAG , "Scan Bar code is ready" );
+                    scanBarcode( view );
+                }
+            }
+        } );
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public
+    boolean onCreateOptionsMenu ( Menu menu ) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate( R.menu.menu_main , menu );
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public
+    boolean onOptionsItemSelected ( MenuItem item ) {
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if ( id == R.id.action_settings ) {
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode != CUSTOMIZED_REQUEST_CODE && requestCode != IntentIntegrator.REQUEST_CODE) {
+    protected
+    void onActivityResult ( int requestCode , int resultCode , Intent data ) {
+        if ( requestCode != CUSTOMIZED_REQUEST_CODE &&
+                requestCode != IntentIntegrator.REQUEST_CODE ) {
             // This is important, otherwise the result will not be passed to the fragment
-            super.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult( requestCode , resultCode , data );
             return;
         }
-        switch (requestCode) {
-            case CUSTOMIZED_REQUEST_CODE: {
-                Toast.makeText(this, "REQUEST_CODE = " + requestCode, Toast.LENGTH_LONG).show();
-                break;
-            }
-            default:
-                break;
+        if ( requestCode == CUSTOMIZED_REQUEST_CODE ) {
+            Toast.makeText( this , "REQUEST_CODE = " + requestCode , Toast.LENGTH_LONG ).show();
         }
 
-        final IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+        final IntentResult result = IntentIntegrator.parseActivityResult( resultCode , data );
 
-        if (result.getContents() != null) {
-            final HomeFragment homeFrag = (HomeFragment)
-                    getSupportFragmentManager().getFragments().get(0).getChildFragmentManager().getFragments().get(0);
-            FIRESTORE.addNewVisit("" + App.userID, result.getContents(),
-                    new Firestore.OnVisitChangeListener() {
-                        @Override
-                        public void OnChangeConfirmed(final Visit visit) {
-                            Toast.makeText(MainActivity.this, visit.toString(), Toast.LENGTH_SHORT).show();
-                            if (homeFrag != null) {
-                                homeFrag.addNewVisit(visit);
+        if ( result.getContents() != null ) {
+            final HomeFragment homeFrag =
+                    ( HomeFragment ) getSupportFragmentManager().getFragments()
+                            .get( 0 )
+                            .getChildFragmentManager()
+                            .getFragments()
+                            .get( 0 );
+            Student student = getGlobStudent();
+            if ( student == null ) {
+                String uID = "-1";
+                firebaseUser = mAuth.getCurrentUser();
+                if ( firebaseUser != null )
+                    uID = firebaseUser.getUid();
+
+                FIRESTORE.checkStudent( new Student( uID ) , true , new Firestore.OnStudentCheckListener() {
+                    @Override
+                    public
+                    void OnStudentChecked ( @Nullable final Student student ) {
+                        setGlobStudent( student );
+                    }
+
+                    @Override
+                    public
+                    void OnStudentCheckFailed ( final String exception ) {
+
+                    }
+
+                    @Override
+                    public
+                    void OnStudentIdentifier ( final Student student ) {
+                        setGlobStudent( student );
+                    }
+                } );
+            }
+            else {
+                FIRESTORE.addNewVisit( new Student( getGlobStudent().getId() ) ,
+                        result.getContents() , new Firestore.OnVisitChangeListener() {
+                            @Override
+                            public
+                            void OnChangeConfirmed ( final Visit visit ) {
+                                if ( homeFrag != null ) {
+                                    homeFrag.addNewVisit( visit );
+                                }
                             }
-                        }
-                    });
-        } else {
-            //Toast.makeText( this , R.string.cancelled , Toast.LENGTH_LONG ).show();
+                        } );
+            }
         }
     }
 
+    @Override
+    public
+    void onBackPressed () {
+        Snackbar snake = Snackbar.make( drawer , getResources().getString( R.string.snackbar ) ,
+                Snackbar.LENGTH_SHORT );
 
+        if ( getBackStackCount() > 0 )
+            super.onBackPressed();
+        else if ( drawer.isDrawerOpen( GravityCompat.START ) ) {
+            drawer.closeDrawer( GravityCompat.START );
+        }
+        else if ( back_pressed + 2000 > System.currentTimeMillis() && !snake.isShown() ) {
+            super.onBackPressed();
+            overridePendingTransition( R.anim.enter_dropin , R.anim.exit_dropout );
+        }
+        else if ( fab.getVisibility() == View.VISIBLE ) {
+            hideFabButtons( fab );
+            snake.addCallback( new Snackbar.Callback() {
+                @Override
+                public
+                void onDismissed ( Snackbar snackbar , int event ) {
+                    if ( event == Snackbar.Callback.DISMISS_EVENT_TIMEOUT ) {
+                        showFabButtons();
+                    }
+                }
+
+                @Override
+                public
+                void onShown ( Snackbar snackbar ) {
+                    hideFabButtons( fab );
+                }
+            } ).setAction( getResources().getString( R.string.close ) , new View.OnClickListener() {
+                @Override
+                public
+                void onClick ( View v ) {
+                    finish();
+                    overridePendingTransition( R.anim.enter_dropin , R.anim.exit_dropout );
+                }
+            } ).show();
+            back_pressed = System.currentTimeMillis();
+        }
+    }
 }
---
+++ app/src/main/java/am/newway/aca/ui/SplashScreen.java
@@ -0,0 +1,171 @@
+package am.newway.aca.ui;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
+
+import am.newway.aca.BaseActivity;
+import am.newway.aca.MainActivity;
+import am.newway.aca.R;
+import am.newway.aca.firebase.Firestore;
+import am.newway.aca.template.Student;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
+
+import static am.newway.aca.ui.SplashScreen.activity.MAIN;
+import static am.newway.aca.ui.SplashScreen.activity.QR;
+
+public
+class SplashScreen extends BaseActivity {
+    private int nAnimation = 0;
+    private String TAG = getClass().getSimpleName();
+
+    @Override
+    public
+    void onCreate ( @Nullable Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+
+        // inflate your main layout here (use RelativeLayout or whatever your root ViewGroup type is
+        final ConstraintLayout mainLayout = ( ConstraintLayout ) this.getLayoutInflater()
+                .inflate( R.layout.activity_splash_screen , null );
+
+        mainLayout.getViewTreeObserver()
+                .addOnGlobalLayoutListener( new ViewTreeObserver.OnGlobalLayoutListener() {
+                    public
+                    void onGlobalLayout () {
+                        mainLayout.getViewTreeObserver().removeOnGlobalLayoutListener( this );
+
+                        startSpringAnimation( findViewById( R.id.text_View1 ) , 1 ,
+                                new OnAnimationListener() {
+                                    @Override
+                                    public
+                                    void OnAnimationEnded () {
+                                        startStudentChecking( nAnimation++ );
+                                    }
+                                } );
+                        startSpringAnimation( findViewById( R.id.text_View2 ) , 2 ,
+                                new OnAnimationListener() {
+                                    @Override
+                                    public
+                                    void OnAnimationEnded () {
+                                        startStudentChecking( nAnimation++ );
+                                    }
+                                } );
+                        startSpringAnimation( findViewById( R.id.text_View3 ) , 3 ,
+                                new OnAnimationListener() {
+                                    @Override
+                                    public
+                                    void OnAnimationEnded () {
+                                        startStudentChecking( nAnimation++ );
+                                    }
+                                } );
+                    }
+                } );
+
+        setContentView( mainLayout );
+        final TextView textView3 = findViewById( R.id.text_View3 );
+        Typeface font = Typeface.createFromAsset( getAssets() , "AGREV4.TTF" );
+        textView3.setTypeface( font );
+    }
+
+    private
+    void startStudentChecking ( int nIterator ) {
+        if ( nIterator == 2 )
+            checkStudent();
+    }
+
+    enum activity {
+        MAIN,
+        QR
+    }
+
+    interface OnAnimationListener {
+        void OnAnimationEnded ();
+    }
+
+    private
+    void startSpringAnimation ( View view , int nType , final OnAnimationListener listener ) {
+        SpringAnimation springAnimation = new SpringAnimation( view ,
+                nType == 1 ? DynamicAnimation.X
+                        : nType == 2 ? DynamicAnimation.Y : DynamicAnimation.ROTATION );
+
+        SpringForce springForce = new SpringForce();
+        springForce.setFinalPosition( nType == 1 ? view.getX() : view.getY() );
+        springForce.setDampingRatio(
+                nType == 1 || nType == 3 ? SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+                        : SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY );
+        springForce.setStiffness( SpringForce.STIFFNESS_VERY_LOW );
+
+        springAnimation.setSpring( springForce );
+        springAnimation.setStartVelocity( 30000f );
+        springAnimation.start();
+
+        springAnimation.addEndListener( new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public
+            void onAnimationEnd ( DynamicAnimation animation , boolean canceled , float value ,
+                    float velocity ) {
+                if ( listener != null )
+                    listener.OnAnimationEnded();
+
+            }
+        } );
+    }
+
+    private
+    void checkStudent () {
+        //նոր աշակերտի ավելացնել կամ ստուգել
+        String uID = "-1";
+        firebaseUser = mAuth.getCurrentUser();
+        if ( firebaseUser != null )
+            uID = firebaseUser.getUid();
+        Student student = new Student( uID );
+
+        FIRESTORE.checkStudent( student , false , new Firestore.OnStudentCheckListener() {
+            @Override
+            public
+            void OnStudentChecked ( final Student student ) {
+                Log.e( TAG , "OnStudentCheckFailed 111" );
+
+                setGlobStudent( student );
+                if ( student != null ) {
+                    Log.e( TAG , "OnStudentCheckFailed not null" );
+                    if ( student.getType() == 1 ) {
+                        startActivity( QR );
+                        return;
+                    }
+                }
+                else
+                    Log.e( TAG , "OnStudentCheckFailed null" );
+
+                startActivity( MAIN );
+            }
+
+            @Override
+            public
+            void OnStudentCheckFailed ( final String exception ) {
+                Log.e( TAG , "OnStudentCheckFailed" );
+                startActivity( MAIN );
+            }
+
+            @Override
+            public
+            void OnStudentIdentifier ( final Student student ) {
+            }
+        } );
+    }
+
+    private
+    void startActivity ( activity nType ) {
+        startActivity( new Intent( SplashScreen.this ,
+                nType == MAIN ? MainActivity.class : QrActivity.class ) );
+        finish();
+    }
+}
